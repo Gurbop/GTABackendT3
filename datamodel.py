7a377c1317913cf2d3e5044bf01c3b0dd7f74a23
Djxnxnx <taruntata2008@gmail.com>
2024-03-28 23:18:02 -0700

    predic tand traingin

--- datamodel.py
+++ datamodel.py
@@ -5,8 +5,10 @@ class datamodel:
     def preprocessing(self):# this is loading in the file stuff into X and Y
         self.X=self.data[['my_trophies','opponent_trophies','my_deck_elixir','op_deck_elixir']]
         self.Y=self.data['my_result']
-    def train(self): # this is actually trianing the model
+    def train(self): 
+        #running the logisitcal regression function
         self.model=LogisticRegression()
         self.model.fit(self.X,self.Y)
-    def predict(self, my_trophies, opponent_trophies, my_deck_elixir,op_deck_elixir): # this is prediciton function
+    def predict(self, my_trophies, opponent_trophies, my_deck_elixir,op_deck_elixir):
+        #predicting the result of victory or defeat based on your trophies and elixir and opponent elixir and trophies
         return self.model.predict([[my_trophies, opponent_trophies, my_deck_elixir,op_deck_elixir]])[0]
